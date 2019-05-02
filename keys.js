180450bb54168b91ba294eb7a84e516ce28d55f9
James Bond <info@desgnnja.com>
2019-05-01 22:07:28 -0500

    Added omdb request and spotify initial reuqests and tested out results with success

--- keys.js
+++ keys.js
@@ -3,6 +3,13 @@ console.log('this is loaded');
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET,
+};
+
+exports.tickemaster = {
   id: process.env.TICKETMASTER_ID,
   secret: process.env.TICKETMASTER_SECRET,
+};
+
+exports.omdb = {
+  id: process.env.OMDB_ID,
 };
--- liri.js
+++ liri.js
@@ -1,29 +1,48 @@
 require("dotenv").config();
 
+const axios = require("axios");
+
 const keys = require("./keys.js");
 
-const spotify = new Spotify(keys.spotify);
+const Spotify = require('node-spotify-api');
 
+const command = process.argv[2];
+const input = process.argv[3];
+console.log(command, input);
 
+if (command === "movie") {
+    getMovie(input)
+} else if (command === "song") {
+    getSong(input)
+}
 
+function getMovie(movie) {
+    if (movie === undefined || movie === "") {
+        movie = "Terminator";
+    }
+    console.log("movie", movie);
+    axios.get(`http://www.omdbapi.com/?t=${movie}&y=&plot=short&apikey=${keys.omdb.id}`).then(
+        function (response) {
+            console.log("The movie is: " + response.data.Title);
+            console.log("The release year is: " + response.data.Year);
+            console.log("The IMDB rating is: " + response.data.imdbRating);
+            console.log(response.data);
+        }
+    );
+}
 
-// ========== date-fns code ==============================
-// var isToday = require('date-fns/is_today')
-// isToday(new Date())
-// //=> true
-
-// ========== This is an example from the Spotify API ==============
-
-//var Spotify = require('node-spotify-api');
-//var spotify = new Spotify({
-//   id: <your spotify client id>,
-//   secret: <your spotify client secret>
-// });
-
-// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-//   if (err) {
-//     return console.log('Error occurred: ' + err);
-//   }
-
-// console.log(data); 
-// });
+function getSong(song) {
+    var spotify = new Spotify({
+        id: keys.spotify.id,
+        secret: keys.spotify.secret,
+    });
+    spotify.search({
+        type: 'track',
+        query: 'All the Small Things'
+    }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        console.log(data.tracks.items[0].name);
+    });
+}
